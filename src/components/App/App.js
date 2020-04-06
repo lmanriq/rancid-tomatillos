f7212378bdc7c480248f66ee397fccf8f815a353
Dan Reardon <dreardon1021@gmail.com>
2020-04-06 16:24:14 -0600

    Setup file structure for App, LoginForm, LoginCon

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+}
+
+export default App;
---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LoginPage.css'
+import LoginForm from '../LoginForm/LoginForm'
+
+const LoginPage = () => {
+  return(
+    <section>
+      <LoginForm />
+      <img src="" />
+    </section>
+  )
+}
