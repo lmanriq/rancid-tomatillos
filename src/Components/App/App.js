09de55c727654123554029141f95b4054556581c
monstaro <monstaro32@gmail.com>
2020-04-06 17:32:36 -0600

    Init commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MovieCardContainer from '../MovieCardContainer/MovieCardContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <MovieCardContainer />
+    </div>
+  );
+}
+
+export default App;
