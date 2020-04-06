357e73fb59d16125b2bf2e70af81e998e9547118
monstaro <monstaro32@gmail.com>
2020-04-06 16:03:47 -0600

    Adds browser router to index js file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
 
+const router = <BrowserRouter><App></App></BrowserRouter>
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {router}
   </React.StrictMode>,
   document.getElementById('root')
 );
