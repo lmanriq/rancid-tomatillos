6418cbfdda722c3943d97185e8864a0dc0b64a33
Trond Makonese <trondation@gmail.com>
2020-04-06 20:06:12 -0600

    Minor updates.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -6,13 +6,13 @@ const NavBar = () => {
     return (
         <section className='nav-bar'>
             <h1 className='site-heading'>Rancid Tomatillos</h1>
-            <img 
+            <img  
             alt="icon-image"
             className="icon" 
             src="https://www.flaticon.com/authors/freepik"
             />
-            <div>
-            <button>
+            <div className='nav-loggin-div'>
+            <button className='nav-loggin-btton'>
                 <p>login</p>
             </button>
             <h4>User Name</h4>
