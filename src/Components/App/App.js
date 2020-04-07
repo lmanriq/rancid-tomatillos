6c9040bb71ff30774ecc68582f625b1194c01bff
Trond Makonese <trondation@gmail.com>
2020-04-06 19:36:44 -0600

    fix typo.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import MovieCardContainer from '../MovieCardContainer/MovieCardContainer';
-import MovieCardContainer from '../NavBar/NavBar';
+import NavBar from '../NavBar/NavBar';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <MovieCardContainer />
     </div>
   );
